72d1fe1680e99eb1d6adcb4741bf4e3df47b1cb5
Hamza Khanzada <m.hamzakhanzada@gmail.com>
2021-03-25 15:41:14 +0500

    Add project files.

---
+++ Hahn.ApplicationProcess.February2021.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hahn.ApplicationProcess.February2021.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
