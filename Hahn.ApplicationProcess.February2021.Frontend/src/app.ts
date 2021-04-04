b01480c653a4d52c9f597d331f14569fead0d948
Hamza Khanzada <m.hamzakhanzada@gmail.com>
2021-04-04 16:14:25 +0500

    Implemented Basic POST from frontend

--- Hahn.ApplicationProcess.February2021.Frontend/src/app.ts
+++ Hahn.ApplicationProcess.February2021.Frontend/src/app.ts
@@ -4,9 +4,21 @@ require('bootstrap');
 
 export class App {
 
+
+  assetName = "Air Conditioner";
+  department = "HQ";
+  countryOfDepartment = "Bangladesh";
+  eMailAdressOfDepartment = "noreply@HQ.com";
+  purchaseDate = "2021-01-01";
+  broken = true;
+
   assetVM: AssetVM;
 
   constructor() {
+    this.loadAssets();
+  }
+
+  private loadAssets() {
     fetch('https://localhost:5001/api/Asset')
       .then(x => x.json())
       .then(x => {
@@ -19,7 +31,36 @@ export class App {
       });
   }
 
+  public async postdata(): Promise<boolean> {
+
+    const payload = {
+      assetName: this.assetName,
+      department: this.department,
+      eMailAdressOfDepartment: this.eMailAdressOfDepartment,
+      countryOfDepartment: this.countryOfDepartment,
+      purchaseDate: this.purchaseDate,
+      broken: this.broken
+    };
+
+    console.log(payload);
+
+    try {
+      await fetch("https://localhost:5001/api/Asset", {
+        method: "post",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload),
+      }).then(response => response.json()).then(x => this.loadAssets());
+
+      return true;
+
+    } catch (e) {
+      console.log("Error: ", e.Message); // handle the error logging however you need.
+      return false;
+    }
+
+  }
 
 
-  public message = 'Hello World!';
 }
